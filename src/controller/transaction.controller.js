f98019ba3a3fead22da88dedc960c5c0c87b5d44
Jonatan Silva <jonatan.tab58@gmail.com>
2021-11-02 22:55:12 -0300

    Feat: Error handling

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -1,9 +1,14 @@
 const repository = require('../repository/transaction.repository')
 
 exports.create = async (req, res) => {
-  let transaction = await repository.create(req.body)
+  try {
+    let transaction = await repository.create(req.body)
 
-  return res.status(201).send(transaction)
+    return res.status(201).send(transaction)
+  } catch(err) {
+    console.error(err)
+    return res.status(500).send(err)
+  }
 }
 
 exports.read = async (req, res, next) => {
