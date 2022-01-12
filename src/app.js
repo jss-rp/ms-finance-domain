278df3e2bd142a0b0b2ec056166ca99e699a9f55
Jonatan Silva <jonatan.tab58@gmail.com>
2022-01-12 19:27:00 -0300

    ➕ Cors dependency

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const routes = require('./route/index')
 
+const cors = require('cors')
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cors())
 app.use('/api', routes)
 
 
