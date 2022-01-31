7b2faa435f6c265f288a890ab43327699563e5fd
Jonatan Silva <jonatan.tab58@gmail.com>
2022-01-30 22:49:18 -0300

    ♻ Remaking controller and adding feats

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -1,24 +1,60 @@
 const { TransactionRepository } = require('../repository')
+const { request } = require('../middle')
 
 exports.create = async (req, res) => {
-  try {
+  request.simple(res, async () => {
     let transaction = await TransactionRepository.create(req.body)
-
     return res.status(201).send(transaction)
-  } catch(err) {
-    console.error(err)
-    return res.status(500).send(err)
-  }
+  })
 }
 
 exports.read = async (req, res) => {
-  let transaction = await TransactionRepository.read(req.params.id)
+  request.simple(res, async () => {
+    let transaction = await TransactionRepository.read(req.params.id)
 
-  return res.status(200).send(transaction)
+    if(!transaction)
+      return res.status(404).send()
+
+    return res.status(200).send(transaction)
+  })
 }
 
-exports.listAll = async (_, res) => {
-  let transactions = await TransactionRepository.listAll()
+exports.readByBank = async (req, res) => {
+  request.simple(res, async () => {
+    let transaction = await TransactionRepository.readByBank(req.params.id)
 
-  return res.status(200).send(transactions)
+    if(!transaction)
+      return res.status(404).send()
+
+    return res.status(200).send(transaction)
+  })
+}
+
+exports.readByCard = async (req, res) => {
+  request.simple(res, async () => {
+    let transaction = await TransactionRepository.readByCard(req.params.id)
+
+    if(!transaction)
+      return res.status(404).send()
+
+    return res.status(200).send(transaction)
+  })
+}
+
+exports.readByTag = async (req, res) => {
+  request.simple(res, async () => {
+    let transaction = await TransactionRepository.readByCard(req.params.id)
+
+    if(!transaction)
+      return res.status(404).send()
+
+    return res.status(200).send(transaction)
+  })
+}
+
+exports.readAll = async (_, res) => {
+  request.simple(res, async () => {
+    let transactions = await TransactionRepository.readAll()
+    return res.status(200).send(transactions)
+  })
 }
