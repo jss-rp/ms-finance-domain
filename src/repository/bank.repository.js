ccc6660ea974fe3e080f04b0312a0f4e8c2a69c5
Jonatan Silva <jonatan.tab58@gmail.com>
2022-01-30 21:54:07 -0300

    ✨ Bank repository

---
+++ src/repository/bank.repository.js
@@ -0,0 +1,7 @@
+const { Bank } = require('../model/bank.model')
+
+exports.create = async (bank) => await Bank.create(bank)
+
+exports.read = async (id) => await Bank.findByPk(id)
+
+exports.readAll = async () => await Bank.findAll()
