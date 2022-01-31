93c562fdd5c3d6322c264847dab9c0f5c268a3bd
Jonatan Silva <jonatan.tab58@gmail.com>
2022-01-30 22:49:59 -0300

    ✨ New transaction routes

--- src/route/transaction.route.js
+++ src/route/transaction.route.js
@@ -5,11 +5,23 @@ const router = express.Router()
 
 router
   .route('/')
-  .get(controller.listAll)
+  .get(controller.readAll)
   .post(controller.create)
 
 router
   .route('/:id')
   .get(controller.read)
+
+router
+  .route('/bank/:id')
+  .get(controller.readByBank)
+
+router
+  .route('/card/:id')
+  .get(controller.readByCard)
+
+router
+  .route('/tag/:id')
+  .get(controller.readByTag)
 
 module.exports = router
