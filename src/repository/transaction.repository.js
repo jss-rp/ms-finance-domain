dcf5191b705a3e527604b75526c1b0c8a8bf42d0
Jonatan Silva <jonatan.tab58@gmail.com>
2022-01-30 21:53:38 -0300

    ✨ New read methods for transactions

--- src/repository/transaction.repository.js
+++ src/repository/transaction.repository.js
@@ -4,4 +4,10 @@ exports.create = async (transaction) => await Transaction.create(transaction)
 
 exports.read = async (id) => await Transaction.findByPk(id)
 
-exports.listAll = async () => await Transaction.findAll()
+exports.readByBank = async (bankId) => await Transaction.findAll({ where: { bankId } })
+
+exports.readByCard = async (cardId) => await Transaction.findAll({ where: { cardId } })
+
+exports.readByTag = async (tagId) => await Transaction.findAll({ where: { tagId } })
+
+exports.readAll = async () => await Transaction.findAll()
